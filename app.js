418d93b6a93dff398cb32cf9c0ff949cb85b6e43
jatinscodingproject <152329190+jatinscodingproject@users.noreply.github.com>
2023-12-26 01:54:59 +0530

    # adding controllers

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const path = require('path');
-
 const express = require('express');
+
 const bodyParser = require('body-parser');
+
+const path = require('path');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorControllers = require('./controllers/error')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorControllers.get404page);
 
 app.listen(3000);
